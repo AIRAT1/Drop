7adcbbf3f4f54bea137a7bf0f930c0ac6b155263
Ayrat <ayrat1@mail.ru>
2018-01-28 22:41:11 +0300

    first release are ready

--- desktop/src/de/android/ayrathairullin/desktop/DesktopLauncher.java
+++ desktop/src/de/android/ayrathairullin/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import de.android.ayrathairullin.Drop;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Drop"; // set title
+		config.width = 800; // set current width
+		config.height = 480; // set current height
 		new LwjglApplication(new Drop(), config);
 	}
 }
