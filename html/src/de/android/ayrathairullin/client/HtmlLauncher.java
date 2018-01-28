e0aba5605c77e59513c07125089138c4eb48ee66
Ayrat <ayrat1@mail.ru>
2018-01-28 19:39:24 +0300

    Initial commit

---
+++ html/src/de/android/ayrathairullin/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package de.android.ayrathairullin.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import de.android.ayrathairullin.Drop;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Drop();
+        }
+}
